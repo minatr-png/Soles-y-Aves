4dd76ee2bef89a9edf0bff362656145a30c4f7ea
Miguel NT <>
2026-09-06 20:06:36 +0200

    Updated icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,8 +11,8 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#eceaea",
     theme_color: "#eceaea",
     icons: [
-      { src: "/icon-192", sizes: "192x192", type: "image/png" },
-      { src: "/icon-512", sizes: "512x512", type: "image/png" },
+      { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
   };
 }
